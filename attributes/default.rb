799d90e08072d8047bbf6a2b2735bd335981e2ec
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-01-31 16:17:31 +0000

    Fixing an error i just introduced!

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default["cookbook_phpbox"]["webserver"] = "nginx"
-default["cookbook_phpbox"]["nginx"]["template_name"] = "nginx_vhost.conf.erb"
-default["cookbook_phpbox"]["nginx"]["template_cookbook"] = "cookbook_phpbox"
-default["cookbook_phpbox"]["nginx"]["listen_port"] = "80"
+default["cookbook_phpbox"]["default_config"]["nginx"]["template_name"] = "nginx_vhost.conf.erb"
+default["cookbook_phpbox"]["default_config"]["nginx"]["template_cookbook"] = "cookbook_phpbox"
+default["cookbook_phpbox"]["default_config"]["nginx"]["listen_port"] = "80"
 
 default["cookbook_phpbox"]["apps"]= []
 
